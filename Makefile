83ce9dfb16aee4419fd043d37162dd762aa9c7a4
contextwin <contextwin@gmail.com>
2019-01-21 19:42:48 +0900

    2019_01_21

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-cmemword: cmemword-0.2.c
-	$(CC) -Wall -O3 -o cmemword cmemword-0.2.c -lncursesw
+cmemword: cmemword-0.3.c
+	$(CC) -Wall -O3 -o cmemword cmemword-0.3.c -lncursesw
--- cmemword-0.3.c
+++ cmemword-0.3.c
@@ -14,17 +14,24 @@
 #define FILES_MAX	256		// 取り扱う出題ファイル数の最大
 #define STRINGS_MAX	1024		// 一つの文字列の最大
 #define QUESTION_MAX	2000		// 一つのファイルの最大出題数
-#define SUCCESS		0
-#define ERROR		1
 #define EDITOR		"vi "
 
+/* global variable */
+WINDOW *question_moniter_wnd,
+       *background_wnd,
+       *text_wnd,
+       *border_text_wnd,
+       *question_text_wnd,
+       *user_input_box_wnd,
+       *user_input_wnd;
+
 /*	struct	*/
-struct filelist_struct {		// 番号とファイル名の組み合わせ
+struct FilelistStruct {		// 番号とファイル名の組み合わせ
 	unsigned char file_number;
 	char file_name[NAME_MAX];
 };
 
-struct answer_and_question {
+struct AnswerAndQuestion {
 	unsigned long	number;
 	unsigned long	rand_key;
 	char answer[STRINGS_MAX];
@@ -32,7 +39,7 @@ struct answer_and_question {
 };
 
 /*	function	*/
-void wait_user_input(WINDOW *win, const char *format, const void *variable_p) {
+void WaitUserInput(WINDOW *win, const char *format, const void *variable_p) {
 
 	delch();
 	mvwprintw(win, 0, 0, "                                                                           ");
@@ -42,27 +49,27 @@ void wait_user_input(WINDOW *win, const char *format, const void *variable_p) {
 	wscanw(win, format, variable_p);
 };
 
-unsigned int fp_read_and_split(FILE *fp, struct answer_and_question *answer_and_question_s) {
+unsigned int FpReadAndSplit(FILE *fp, struct AnswerAndQuestion *answer_and_question_s) {
 
-	unsigned char cnt = 0,
-		      cnt1 = 0;
+	unsigned char cnt1 = 0,
+		      cnt2 = 0;
 
 	unsigned int question_max = 0;
 
-	for (cnt = 0; !feof(fp); cnt++) {
-		answer_and_question_s[cnt].number = cnt + 1;
-		for(cnt1 = 0 ;; cnt1++){
-			answer_and_question_s[cnt].answer[cnt1] = getc(fp);
-			if (answer_and_question_s[cnt].answer[cnt1] == '\t') {
-				answer_and_question_s[cnt].answer[cnt1] = '\0';
+	for (cnt1 = 0; !feof(fp); cnt1++) {
+		answer_and_question_s[cnt1].number = cnt1 + 1;
+		for(cnt2 = 0 ;; cnt2++){
+			answer_and_question_s[cnt1].answer[cnt2] = getc(fp);
+			if (answer_and_question_s[cnt1].answer[cnt2] == '\t') {
+				answer_and_question_s[cnt1].answer[cnt2] = '\0';
 				break;
 			}
 			if (feof(fp)) break; 
 		}
-		for (cnt1 = 0; answer_and_question_s[cnt].question[cnt1] != '\n'; cnt1++) {
-			answer_and_question_s[cnt].question[cnt1] = getc(fp);
-			if (answer_and_question_s[cnt].question[cnt1] == '\n') {
-				answer_and_question_s[cnt].question[cnt1] = '\0';
+		for (cnt2 = 0; answer_and_question_s[cnt1].question[cnt2] != '\n'; cnt2++) {
+			answer_and_question_s[cnt1].question[cnt2] = getc(fp);
+			if (answer_and_question_s[cnt1].question[cnt2] == '\n') {
+				answer_and_question_s[cnt1].question[cnt2] = '\0';
 				question_max++;
 				break;
 			}
@@ -71,27 +78,57 @@ unsigned int fp_read_and_split(FILE *fp, struct answer_and_question *answer_and_
 	}
 	fclose(fp);
 	return question_max;
-}
+};
+
+void Question(unsigned short number_of_start_question, unsigned short number_of_end_question, struct AnswerAndQuestion *answer_and_question_s){
+
+	unsigned char	cnt1 = 0, cnt2 = 0;
+	char		user_input_answer[STRINGS_MAX];
+
+	for (cnt1 = number_of_start_question, cnt2 = 1; cnt1 < number_of_end_question; cnt1++, cnt2++) {
+		wclear(question_text_wnd);
+		mvwprintw(question_text_wnd, 0, 0, "question : #%hhu\tline number : #%lu", cnt2, answer_and_question_s[cnt1].number);
+		mvwprintw(question_text_wnd, 1, 0, "Q: %s\n", answer_and_question_s[cnt1].question);
+		wrefresh(question_text_wnd);
+		WaitUserInput(user_input_wnd, "%[^\t\n]", user_input_answer);
+
+		if (!strcmp(answer_and_question_s[cnt1].answer, user_input_answer)) {
+			wclear(user_input_wnd);
+			mvwprintw(user_input_wnd, 2, 0, "correct!!");
+			mvwprintw(user_input_wnd, 3, 0, "A: %s", answer_and_question_s[cnt1].answer);
+			wrefresh(user_input_wnd);
+		} else {
+			wclear(user_input_wnd);
+			mvwprintw(user_input_wnd, 2, 0, "miss!!");
+			mvwprintw(user_input_wnd, 3, 0, "A: %s\n\n", answer_and_question_s[cnt1].answer);
+			wrefresh(user_input_wnd);
+			cnt1--;
+			cnt2--;
+		}
+	}
+
+};
 
 
 /*	Main method	*/
 int main(int argc,char** argv)
 {
 	/*	variable	*/
-	WINDOW *question_moniter_wnd,
+/*	WINDOW *question_moniter_wnd,
 	       *background_wnd,
 	       *text_wnd,
 	       *border_text_wnd,
 	       *question_text_wnd,
 	       *user_input_box_wnd,
 	       *user_input_wnd;
+*/
+	DIR	*files_dir;					// Files ディレクトリ
+	FILE	*reading_fp;					// 出題ファイルを格納
 
-	DIR *files_dir;						// Files ディレクトリ
-	FILE *reading_fp;					// 出題ファイルを格納
-	struct dirent *files_dp;				// ディレクトリのデータを扱う構造体
-	struct filelist_struct filelist_s[FILES_MAX];		// 出題ファイル一覧の構造体
-	struct answer_and_question answer_and_question_s[QUESTION_MAX], // 出題番号,解答,問題,の構造体
-	       answer_and_question_tmp;				// sort用
+	struct dirent			*files_dp;				// ディレクトリのデータを扱う構造体
+	struct FilelistStruct		filelist_s[FILES_MAX];			// 出題ファイル一覧の構造体
+	struct AnswerAndQuestion	answer_and_question_s[QUESTION_MAX],	// 出題番号,解答,問題,の構造体
+	       answer_and_question_tmp;						// sort用
 
 	char files_dir_path[PATH_MAX], 				// 出題ファイルのパス
 	     user_input_y_or_n,					// ユーザ入力の y か n を格納する
@@ -112,12 +149,12 @@ int main(int argc,char** argv)
 	strcat(command_line_str, EDITOR);
 	if (NULL == getcwd(files_dir_path, PATH_MAX)) {
 		printf("カレントディレクトリの取得に失敗しました。\n");
-		exit(ERROR);
+		exit(EXIT_FAILURE);
 	}
 
 	if (PATH_MAX < (strlen(files_dir_path) + strlen(FILES_DIR_NAME))) {
 		printf("出題ファイルまでのパスの長さが PATE_MAX(4096) 以上です。");
-		exit(ERROR);
+		exit(EXIT_FAILURE);
 	} else {
 		strcat(files_dir_path, FILES_DIR_NAME);
 	}
@@ -126,13 +163,13 @@ int main(int argc,char** argv)
 
 		if (!(files_dir = opendir(files_dir_path))) {
 			printf("%s ディレクトリが存在しません。\n", files_dir_path);
-			exit(ERROR);
+			exit(EXIT_FAILURE);
 		}
 
 		// 出題ファイルが格納されているディレクトリまで移動(エラー処理書く)
 		if ((-1) == chdir(files_dir_path)) {
 			printf("%s ディレクトリへの移動に失敗しました。\n", files_dir_path);
-			exit(ERROR);
+			exit(EXIT_FAILURE);
 		};
 
 		for (files_dp = readdir(files_dir), cnt = 0; files_dp != NULL; files_dp = readdir(files_dir)){
@@ -149,7 +186,7 @@ int main(int argc,char** argv)
 
 		if (0 == cnt) {
 			printf("出題用のファイルが存在しません。\n%s.\nに出題用のファイルを作成してください。\n", files_dir_path);
-			exit(ERROR);
+			exit(EXIT_FAILURE);
 		};
 
 		number_of_files = cnt;		// 出題ファイルの量を代入
@@ -233,7 +270,7 @@ int main(int argc,char** argv)
                 mvwaddstr(question_text_wnd, 4, 0, "[  2] 出題ファイルを編集する。\n");
 		wrefresh(question_text_wnd);
 
-                wait_user_input(user_input_wnd, "%hd", &user_input_num);
+                WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 
 		if (1 == user_input_num) {
 			/*	出題ファイル選択ループ	*/
@@ -251,7 +288,7 @@ int main(int argc,char** argv)
 				}
 				wrefresh(question_text_wnd);
 
-				wait_user_input(user_input_wnd, "%hd", &user_input_num);
+				WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 
 				/*	入力エラーチェック	*/
 				if (user_input_num > number_of_files) {
@@ -270,7 +307,7 @@ int main(int argc,char** argv)
 				endwin();
 				printf("ファイルの読み込みに失敗しました。\n");
 				printf("file open error.\n");
-				exit(ERROR);
+				exit(EXIT_FAILURE);
 			}
 
 		} else if (2 == user_input_num) {
@@ -283,7 +320,7 @@ int main(int argc,char** argv)
 				mvwaddstr(question_text_wnd, 2, 0, "[  1] 既存のファイルを編集する。\n");
 				mvwaddstr(question_text_wnd, 3, 0, "[  2] 新規ファイルを作成する。\n");
 				wrefresh(question_text_wnd);
-				wait_user_input(user_input_wnd, "%hd", &user_input_num);
+				WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 
 				if (1 == user_input_num) {
 
@@ -298,7 +335,7 @@ int main(int argc,char** argv)
 							mvwprintw(question_text_wnd, cnt + 3, 0, "[%3d] %s\n", filelist_s[cnt].file_number, filelist_s[cnt].file_name);
 						}
 						wrefresh(question_text_wnd);
-						wait_user_input(user_input_wnd, "%hd", &user_input_num);
+						WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 						/*	入力エラーチェック	*/
 						if (user_input_num > number_of_files) {
 							wclear(user_input_wnd);
@@ -336,11 +373,11 @@ int main(int argc,char** argv)
 		} else if (0 == user_input_num) {
 			endwin();
 			printf("bye.\n");
-			exit(SUCCESS);
+			exit(EXIT_SUCCESS);
 		} else {
 			endwin();
 			printf("1か2以外が入力されました。\n");
-			exit(ERROR);
+			exit(EXIT_FAILURE);
 		}
 
 		user_input_num = 0;
@@ -355,10 +392,10 @@ int main(int argc,char** argv)
 			mvwaddstr(question_text_wnd, 4, 0, "[  3] 解答の文字数が少ない順に出題する。");
 			mvwaddstr(question_text_wnd, 5, 0, "[  4] 解答の文字数が多い順に出題する。");
 			wrefresh(question_text_wnd);
-			wait_user_input(user_input_wnd, "%hu", &user_input_num);
+			WaitUserInput(user_input_wnd, "%hu", &user_input_num);
 
 			if (1 == user_input_num) {
-				question_max = fp_read_and_split(reading_fp, answer_and_question_s);
+				question_max = FpReadAndSplit(reading_fp, answer_and_question_s);
 				break;
 			/*	random		*/
 			} else if (2 == user_input_num) {
@@ -401,7 +438,7 @@ int main(int argc,char** argv)
 
 			/*	In order of decreasing number of characters	*/
 			} else if (3 == user_input_num) {
-				question_max = fp_read_and_split(reading_fp, answer_and_question_s);
+				question_max = FpReadAndSplit(reading_fp, answer_and_question_s);
 				for (cnt = 0; cnt < question_max; cnt++) {
 					for (cnt1 = 1; (cnt + cnt1) < question_max; cnt1++) {
 						if (strlen(answer_and_question_s[cnt].answer) > strlen(answer_and_question_s[cnt + cnt1].answer)) {
@@ -425,7 +462,7 @@ int main(int argc,char** argv)
 				break;
 			/*	In descending order of the number of characters		*/
 			} else if (4 == user_input_num) {
-				question_max = fp_read_and_split(reading_fp, answer_and_question_s);
+				question_max = FpReadAndSplit(reading_fp, answer_and_question_s);
 
 				for (cnt = 0; cnt < question_max; cnt++) {
 					for (cnt1 = 1; (cnt + cnt1) < question_max; cnt1++) {
@@ -464,7 +501,7 @@ int main(int argc,char** argv)
 			mvwprintw(question_text_wnd, 0, 0, "出題数：%d", question_max);
 			mvwaddstr(question_text_wnd, 1, 0, "全問出題しますか?\n(y/n)");
 			wrefresh(question_text_wnd);
-			wait_user_input(user_input_wnd, "%c", &user_input_y_or_n);
+			WaitUserInput(user_input_wnd, "%c", &user_input_y_or_n);
 
 			if ('y' == user_input_y_or_n) {
 				number_of_start_question = 0;
@@ -481,7 +518,7 @@ int main(int argc,char** argv)
 						mvwaddstr(question_text_wnd, 1, 0, "何問目から出題しますか?");
 						mvwaddstr(question_text_wnd, 2, 0 ,"数値を入力して下さい。");
 						wrefresh(question_text_wnd);
-						wait_user_input(user_input_wnd, "%hd", &user_input_num);
+						WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 						
 						/*	入力エラーチェック	*/
 						if (user_input_num > question_max) {
@@ -489,12 +526,12 @@ int main(int argc,char** argv)
 							wprintw(user_input_wnd, "実際の問題の量以上の値か、負の値が入力されました。");
 							mvwaddstr(user_input_wnd, 1, 0, "A number greater than the actual number of file was entered.\n");
 							wrefresh(user_input_wnd);
-							wait_user_input(user_input_wnd, "%hd", &user_input_num);
+							WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 						} else if (0 == user_input_num) {
 							wclear(user_input_wnd);
 							wprintw(user_input_wnd, "0問目は存在しません。1以上の数値を入力して下さい。");
 							wrefresh(user_input_wnd);
-							wait_user_input(user_input_wnd, "%hd", &user_input_num);
+							WaitUserInput(user_input_wnd, "%hd", &user_input_num);
 						} else {
 							break;
 						}
@@ -510,7 +547,7 @@ int main(int argc,char** argv)
 						mvwaddstr(question_text_wnd, 2, 0, "何問目まで出題しますか?");
 						mvwaddstr(question_text_wnd, 3, 0 ,"数値を入力して下さい。");
 						wrefresh(question_text_wnd);
-						wait_user_input(user_input_wnd, "%hd", &number_of_end_question);
+						WaitUserInput(user_input_wnd, "%hd", &number_of_end_question);
 
 						/*	入力エラーチェック	*/
 						if (number_of_end_question > question_max) {
@@ -518,13 +555,13 @@ int main(int argc,char** argv)
 							wprintw(user_input_wnd, "実際の問題の量以上の値か、負の値が入力されました。");
 							mvwaddstr(user_input_wnd, 1, 0, "A number greater than the actual number of file was entered.");
 							wrefresh(user_input_wnd);
-							wait_user_input(user_input_wnd, "%c", &user_input_num);
+							WaitUserInput(user_input_wnd, "%c", &user_input_num);
 						} else if (number_of_start_question > number_of_end_question) {
 							wclear(user_input_wnd);
 							wprintw(user_input_wnd, "出題開始行番号より小さい数値が入力されました。");
 							mvwaddstr(user_input_wnd, 1, 0, "A number smaller than thaline number at which the question is to be stated has been entered.");
 							wrefresh(user_input_wnd);
-							wait_user_input(user_input_wnd, "%c", &user_input_num);
+							WaitUserInput(user_input_wnd, "%c", &user_input_num);
 						} else {
 							break;
 						}
@@ -541,7 +578,7 @@ int main(int argc,char** argv)
                                                 mvwaddstr(question_text_wnd, 1, 0, "何問出題しますか?");
                                                 mvwaddstr(question_text_wnd, 2, 0, "数値を入力して下さい\n");
 						wrefresh(question_text_wnd);
-						wait_user_input(user_input_wnd, "%hd", &number_of_end_question);
+						WaitUserInput(user_input_wnd, "%hd", &number_of_end_question);
                                                 /*      入力エラーチェック      */
                                                 if (number_of_end_question > question_max) {
 							wclear(user_input_wnd);
@@ -563,7 +600,7 @@ int main(int argc,char** argv)
 				wclear(user_input_wnd);
 				wprintw(user_input_wnd, "y(yes) か n(no) を入力して下さい。");
 				wrefresh(user_input_wnd);
-				wait_user_input(user_input_wnd, "%c", user_input_answer);
+				WaitUserInput(user_input_wnd, "%c", user_input_answer);
 				
 			}
 		}
@@ -574,7 +611,7 @@ int main(int argc,char** argv)
 			mvwprintw(question_text_wnd, 0, 0, "question : #%hhu\tline number : #%lu", cnt1, answer_and_question_s[cnt].number);
 			mvwprintw(question_text_wnd, 1, 0, "Q: %s\n", answer_and_question_s[cnt].question);
 			wrefresh(question_text_wnd);
-			wait_user_input(user_input_wnd, "%[^\t\n]", user_input_answer);
+			WaitUserInput(user_input_wnd, "%[^\t\n]", user_input_answer);
 
 			if (!strcmp(answer_and_question_s[cnt].answer, user_input_answer)) {
 				wclear(user_input_wnd);
@@ -594,18 +631,24 @@ int main(int argc,char** argv)
 		for (;;) {
 
 			wclear(question_text_wnd);
-			wprintw(question_text_wnd, "出題が終わりました。プログラムを終了しますか?(y/n)\n");
+			wprintw(question_text_wnd, "出題が終わりました。プログラムを終了しますか?\n");
+			wprintw(question_text_wnd, "r でもう一度同じ出題を繰り返します。(y/n/r)\n");
 			wrefresh(question_text_wnd);
-			wait_user_input(user_input_wnd, "%c", &user_input_y_or_n);
+			WaitUserInput(user_input_wnd, "%c", &user_input_y_or_n);
 
 			if ('y' == user_input_y_or_n) {
+				endwin();
+				printf("bye.\n");
+				exit(EXIT_SUCCESS);
 				break;
 			} else if ('n' == user_input_y_or_n) {
 				question_max = 0;
 				break;
+			} else if ('r' == user_input_y_or_n) {
+				Question(number_of_start_question, number_of_end_question, answer_and_question_s);
 			} else {
 				wclear(question_text_wnd);
-				wprintw(question_text_wnd, "y(yes) か n(no) を入力して下さい。\n\n");
+				wprintw(question_text_wnd, "y(yes) か n(no) か r(retry) を入力して下さい。\n\n");
 				wrefresh(question_text_wnd);
 			}
 		}
@@ -613,5 +656,6 @@ int main(int argc,char** argv)
 	}
 
 	endwin();
-	exit(SUCCESS);
+	printf("bye.\n");
+	exit(EXIT_SUCCESS);
 };
